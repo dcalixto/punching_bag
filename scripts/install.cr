dce9d470cb39033844dde3bed57c311ca65c4226
dcalixto <calixtodaniel@gmail.com>
2024-11-30 03:13:35 -0300

    Enhancing the shard installation process.

--- scripts/install.cr
+++ scripts/install.cr
@@ -1,5 +1,7 @@
 require "../src/punching_bag"
+require "file_utils"
 
+# Initialize the database
 DB.open(PunchingBag::Configuration.database_url) do |db|
   db.exec "CREATE TABLE IF NOT EXISTS punches (
     id INTEGER PRIMARY KEY,
@@ -10,4 +12,25 @@ DB.open(PunchingBag::Configuration.database_url) do |db|
     average_time DATETIME NOT NULL,
     hits INTEGER DEFAULT 1
   )"
+end
+
+# Create the bin directory and punching_bag.cr script
+FileUtils.mkdir_p("./bin")
+setup_path = "./bin/punching_bag.cr"
+
+unless File.exists?(setup_path)
+  File.write(setup_path, <<-CODE)
+#!/usr/bin/env crystal
+
+require "punching_bag/cli"
+
+if ARGV.size > 0
+  PunchingBag::CLI.run(ARGV[0])
+else
+  puts "Usage: punching_bag <command>"
+end
+  CODE
+  puts "Setup complete. File created at #{setup_path}"
+else
+  puts "Setup already completed. File exists at #{setup_path}"
 end
--- src/cli.cr
+++ src/cli.cr
@@ -17,14 +17,14 @@ class PunchingBag::CLI
 
     unless File.exists?(setup_path)
       File.write(setup_path, <<-CODE)
-#!/usr/bin/env crystal
-
-require "punching_bag"
-
-if ARGV.size > 0
-PunchingBag::CLI.run(ARGV[0])
-end
-      CODE
+      #!/usr/bin/env crystal
+    
+      require "punching_bag/cli"
+    
+      if ARGV.size > 0
+        PunchingBag::CLI.run(ARGV[0])
+      end
+    CODE
       puts "Setup complete. File created at #{setup_path}"
     else
       puts "Setup already completed."
