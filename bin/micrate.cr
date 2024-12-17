c92d81bb0195033e899517dbd29a27b07d55563f
dcalixto <calixtodaniel@gmail.com>
2024-12-17 13:09:49 -0300

    Sets up a dedicated migration runner

--- bin/micrate.cr
+++ bin/micrate.cr
@@ -1,6 +1,8 @@
-require "micrate"
 require "db"
+require "pg"
+require "micrate"
 
-DATABASE_URL = ENV["DATABASE_URL"]?
+DATABASE_URL = ENV["DATABASE_URL"]? || "postgres://daniel:password@localhost/kemal_db"
 Micrate::DB.connection_url = DATABASE_URL
+
 Micrate::Cli.run
